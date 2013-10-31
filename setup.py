46eae8f70a3eec8ecc613089b797564b2b289974
Nathan West <Lucretiel@gmail.com>
2013-10-30 22:23:32 -0400

    Removed Python 3.2 classifier
    Currently, Dispatcher depends on `inspect.signature`, which was added in python 3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Dispatching",
-    version="1.0",
+    version="1.0.1",
     py_modules=['dispatching',],
     test_suite='test',
     platforms='any',
@@ -21,7 +21,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
